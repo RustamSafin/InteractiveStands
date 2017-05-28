304ffd487ac5a1d2a75787e17df055d12f357e2f
RustamSafin <last.safin@yandex.ru>
2017-05-29 00:45:56 +0300

    bbbsadsa

--- src/main/java/com/prover/prover/utils/Constants.java
+++ src/main/java/com/prover/prover/utils/Constants.java
@@ -4,6 +4,6 @@ package com.prover.prover.utils;
  * Created by Admin on 22.05.2017.
  */
 public class Constants {
-    public static final int STANDS_LIMIT = 1;
+    public static final int STANDS_LIMIT = 20;
     public static final String UPLOADED_FOLDER = "/";
 }
